a85c427a976868d37f1a9cc4a4d24a64c95bafa1
Abuasim2 <ittech.uau@gmail.com>
2022-09-11 16:23:09 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,9 @@
 /**
 * Description: main - Print out a string to stdout. 
 *Return: 0 if success. 
-*/   
+*/  
 int main(void)
 {
-iprintf("with proper grammar, but the outcome is a piece of
-art,\n");
+iprintf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
