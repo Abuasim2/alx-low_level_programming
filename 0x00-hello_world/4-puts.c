9235ff318df1a3c38ef63acd54e3fd82fde35d54
Abuasim2 <ittech.uau@gmail.com>
2022-09-10 18:16:54 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* Description: main - print out a string to stdout.
-* Return: 0 if success.
+*Description: main - prints out a string to stdout.
+*Return: 0 if success.
 */
-int main(void)
+nt main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} 
+}
