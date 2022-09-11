ef704e18ab5f91ecec103d4eb453b126dfbaaa71
Abuasim2 <ittech.uau@gmail.com>
2022-09-10 18:52:52 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+*/
+int main(void)
+return (0);
+* main - Prints a text
+/**
+printf("with proper grammar, but the outcome is a piece of
+* Return: Always 0 (Success)
+{
+art,\n");
+*
+}
